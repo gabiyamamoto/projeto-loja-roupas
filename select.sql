854d2c13d03d898049246afe6f77ba9de49ed507
Danilo Jorge <danilojmini2@gmail.com>
2025-12-01 08:01:32 -0300

    Atualizando os SELECTS

--- select.sql
+++ select.sql
@@ -1,60 +1,28 @@
 SELECT
-    c.nome_cliente,
-    v.data_venda,
-    p.nome_produtos,
-    p.tamanho,
-    p.cor,
-    iv.quantidade
-FROM
-    VENDAS v
-    INNER JOIN CLIENTES c ON v.cliente_id = c.cliente_id
-    INNER JOIN FK_ITENS_VENDA iv ON v.venda_id = iv.venda_id
-    INNER JOIN PRODUTOS p ON iv.produto_id = p.produto_id
-WHERE
-    v.venda_id = 1;
+    CLIENTES.nome_cliente,
+    VENDAS.data_venda,
+    PRODUTOS.nome_produtos,
+    PRODUTOS.tamanho,
+    PRODUTOS.cor,
+    ITENS_VENDA.quantidade
+FROM VENDAS
+INNER JOIN CLIENTES ON VENDAS.cliente_id = CLIENTES.cliente_id
+INNER JOIN ITENS_VENDA ON VENDAS.venda_id = ITENS_VENDA.venda_id
+INNER JOIN PRODUTOS ON ITENS_VENDA.produto_id = PRODUTOS.produto_id
+WHERE VENDAS.venda_id = 1;
 
 SELECT
-    p.nome_produtos,
-    p.quantidade_estoque,
-    c.nome_categoria
-FROM
-    PRODUTOS p
-    INNER JOIN CATEGORIAS c ON p.categoria_id = c.categoria_id
-WHERE
-    c.nome_categoria = 'Camisetas' AND p.quantidade_estoque < 10;
+    PRODUTOS.nome_produtos,
+    PRODUTOS.quantidade_estoque,
+    CATEGORIAS.nome_categoria
+FROM PRODUTOS
+INNER JOIN CATEGORIAS ON PRODUTOS.categoria_id = CATEGORIAS.categoria_id
+WHERE CATEGORIAS.nome_categoria = 'Camisetas'
+  AND PRODUTOS.quantidade_estoque < 10;
 
 SELECT
-    c.nome_cliente,
-    v.data_venda
-FROM
-    CLIENTES c
-    LEFT JOIN VENDAS v ON c.cliente_id = v.cliente_id
-ORDER BY
-    c.nome_cliente;
-
-CREATE VIEW Vw_Inventario_Completo AS
-SELECT
-    p.nome_produtos,
-    c.nome_categoria,
-    p.tamanho,
-    p.cor,
-    p.preco,
-    p.quantidade_estoque
-FROM
-    PRODUTOS p
-    INNER JOIN CATEGORIAS c ON p.categoria_id = c.categoria_id;
-
-CREATE VIEW Vw_Historico_De_Vendas AS
-SELECT
-    v.data_venda,
-    c.nome_cliente,
-    p.nome_produtos,
-    p.tamanho,
-    p.cor,
-    iv.quantidade,
-    iv.preco_unitario
-FROM
-    VENDAS v
-    INNER JOIN CLIENTES c ON v.cliente_id = c.cliente_id
-    INNER JOIN FK_ITENS_VENDA iv ON v.venda_id = iv.venda_id
-    INNER JOIN PRODUTOS p ON iv.produto_id = p.produto_id;
+    CLIENTES.nome_cliente,
+    VENDAS.data_venda
+FROM CLIENTES
+LEFT JOIN VENDAS ON CLIENTES.cliente_id = VENDAS.cliente_id
+ORDER BY CLIENTES.nome_cliente;
